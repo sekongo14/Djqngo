fbec129e01fd99d3fafe6deb6f441550d32d5ee5
sekongo14 <sekongoismael0@gmail.com>
2023-10-03 22:29:39 +0000

    ajout

--- projet1/mangalib/views.py
+++ projet1/mangalib/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
 
 # Create your views here.
 
 def index(request):
-    return HttpResponse("bienvenue")
+    context = {'message': 'hello !'}
+    template = loader.get_template("mangalib/index.html")
+    return HttpResponse(template.render(context, request) )
